2fdffd2ba0a7ca7ab9ecfc235735f9906bcefea4
HadeelAlshaer94 <Hadeelalshaer94@outlook.com>
2021-08-01 22:30:50 +0300

    add-feedback-local-storge

--- JS/app.js
+++ JS/app.js
@@ -26,3 +26,38 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+// review
+// let Review = document.getElementById('Review');
+// let userReview = document.createElement('p');
+// Review.appendChild(userReview);
+// console.log(Review);
+let storgeReview = JSON.parse(localStorage.getItem('userfeedback'));
+console.log(storgeReview);
+
+
+function getStorageData() {
+
+  let cartItem = JSON.parse(localStorage.getItem('userfeedback'));
+  console.log(cartItem);
+  if (cartItem !== null) {
+    for (let i = 0; i < cartItem.length; i++) {
+      let Review = document.getElementById('Review');
+      let userReview = document.createElement('p');
+      Review.appendChild(userReview);
+      userReview.textContent = cartItem[i].firstName + "  " + cartItem[i].lastName;
+
+      let feedback = document.createElement('p');
+      Review.appendChild(feedback);
+      feedback.textContent = cartItem[i].feedback;
+      
+      
+
+console.log(userReview);
+    }
+
+  }
+
+}
+getStorageData();
